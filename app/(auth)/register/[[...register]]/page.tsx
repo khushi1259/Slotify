f0e07a18b8ecad44eb778361c5179580db4ff2f0
khushi1259 <khushimehta1259@gmail.com>
2026-03-06 18:02:47 +0530

    Add signInUrl prop to SignUp component

--- app/(auth)/register/[[...register]]/page.tsx
+++ app/(auth)/register/[[...register]]/page.tsx
@@ -2,17 +2,18 @@ import { SignUp } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function RegisterPage() {
-    return (
-        <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
-             <Image
-                src='/assets/slotify.svg'
-                width={100}
-                height={100}
-                alt="Logo"
-            />  
-            <div className="mt-3">
-                <SignUp/> 
-            </div>
-        </main>        
-    )
+  return (
+    <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
+      <Image
+        src="/assets/slotify.svg"
+        width={100}
+        height={100}
+        alt="Logo"
+      />
+
+      <div className="mt-3">
+        <SignUp signInUrl="/sign-in" />
+      </div>
+    </main>
+  );
 }
