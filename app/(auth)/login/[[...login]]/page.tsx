0017b7d58bf7f14a3008bb45fb247682c2627b81
khushi1259 <khushimehta1259@gmail.com>
2026-03-06 17:55:03 +0530

    Update page.tsx

--- app/(auth)/login/[[...login]]/page.tsx
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -1,17 +1,15 @@
-import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function LoginPage(){
 return(
-    <main className="flex flex-col items-center p-5 gap-10 animate-fade-in" >
+    <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
         <Image
             src='/assets/slotify.svg'
             width={300}
             height={300}
             alt="Logo"
         />
+
         <div className="mt-3">
-            <SignIn/>
+            <SignIn signUpUrl="/sign-up" />
         </div>
     </main>
 )
